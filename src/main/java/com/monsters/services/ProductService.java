ae3e21b571e317a30f01a52f429a525d85058ec7
J-uds <206530139+J-uds@users.noreply.github.com>
2025-06-16 14:26:05 +0200

    feat(services): add get, post and update methods to product and get and post to review

--- src/main/java/com/monsters/services/ProductService.java
+++ src/main/java/com/monsters/services/ProductService.java
@@ -2,7 +2,9 @@ package com.monsters.services;
 
 import com.monsters.models.Product;
 import com.monsters.repositories.ProductRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,7 +16,30 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<Product> getAllProductService() {
+    public List<Product> getAllProduct() {
         return productRepository.findAll();
     }
+
+    public Product getByIdProduct(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+    }
+
+    public Product addProduct(Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    /*public Product updateProductById(Long id, Product updatedProduct) {
+        Product toUpdateProduct = getByIdProduct(id);
+        toUpdateProduct.setName(updatedProduct.getName());
+        toUpdateProduct.setPrice(updatedProduct.getPrice());
+        toUpdateProduct.setImageUrl(updatedProduct.getImageUrl());
+        toUpdateProduct.setRating(updatedProduct.getRating());
+        toUpdateProduct.setReviewCount(updatedProduct.getReviewCount());
+
+        return productRepository.save(toUpdateProduct);
+    }*/
 }
--- src/main/java/com/monsters/services/ReviewService.java
+++ src/main/java/com/monsters/services/ReviewService.java
@@ -2,7 +2,9 @@ package com.monsters.services;
 
 import com.monsters.models.Review;
 import com.monsters.repositories.ReviewRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,7 +16,15 @@ public class ReviewService {
         this.reviewRepository = reviewRepository;
     }
 
-    public List<Review> getAllReviewService() {
+    public List<Review> getAllReview() {
         return reviewRepository.findAll();
+    }
+
+    public Review getByIdReview(Long id) {
+        return reviewRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Review not found"));
+    }
+
+    public Review addReview(Review newReview) {
+        return reviewRepository.save(newReview);
     }
 }
