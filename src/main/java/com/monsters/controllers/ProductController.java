d7ddd4bf3775ac53c256c938db3166e0253a7245
J-uds <206530139+J-uds@users.noreply.github.com>
2025-06-16 13:01:59 +0200

    feat(controllers): add package controllers, review and product controller, connection with services and getall methods

---
+++ src/main/java/com/monsters/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.monsters.controllers;
+
+import com.monsters.models.Product;
+import com.monsters.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<List<Product>> getAllProductController() {
+        List<Product> products = productService.getAllProductService();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+}
