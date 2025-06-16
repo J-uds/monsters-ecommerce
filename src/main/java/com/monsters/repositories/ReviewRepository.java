a19c9eb8693312952525beb0b5293e373ea14ade
J-uds <206530139+J-uds@users.noreply.github.com>
2025-06-16 11:58:07 +0200

    feat(repositories): add repository interfaces product and review

---
+++ src/main/java/com/monsters/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.monsters.repositories;
+
+import com.monsters.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
